ca5527e9a2b9002e16b84098b576d4dc2ecad137
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-06-14 20:27:26 -0400

    dd

---
+++ ForegroundJobScheduler/pythonscripts/covertpdtocsv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mp
+
+if __name__ == '__main__':
+    file_list = [1227464, 1926479]
+    for num in file_list:
+        trace_dataframe = pd.read_pickle("C:\\Users\\carlo\\PycharmProjects\\ForegroundJobScheduler\\datasets\\alldata\\" + str(num) + ".pd", compression=None)
+        trace_dataframe.to_csv(path_or_buf="C:\\Users\\carlo\\PycharmProjects\\ForegroundJobScheduler\\datasets\\csvalldata\\" + str(num) + ".csv")
