9ec5d41ef6042b7591e159a9c9953ad346739378
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-07-26 21:53:56 -0400

    Updates

--- ForegroundJobScheduler/rscript/svt_arma_stationary.r
+++ ForegroundJobScheduler/rscript/svt_arma_stationary.r
@@ -262,10 +262,8 @@ svt_stationary_model <- function(dataset, initial_train_size, job_length=5, cpu_
 
 ## Read back ground job pool
 
-bg_job_pool_names <- read.csv("C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//pythonscripts//list of sampled background jobs.csv")[,1]
-bg_job_pool <- sub(".pd", "", bg_job_pool_names)
-bg_job_pool <- sample(bg_job_pool, 100, replace = FALSE)
-bg_jobs_path = "C://Users//carlo//Documents//datasets//csvalldata//background_jobs//"
+bg_job_pool <- read.csv("C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//pythonscripts//list of sampled 100 bg jobs.csv")[,2]
+bg_jobs_path = "C://Users//carlo//Documents//sample background jobs//"
 
 data_matrix <- matrix(nrow = 4000, ncol = 0)
 for (job_num in bg_job_pool) {
@@ -283,12 +281,12 @@ for (j in 1:ncol(data_matrix)) {
 for (job_length in c(1, 12)) {
   print(paste("Job_length", job_length))
   
-  output <- svt_stationary_model(dataset=data_matrix, job_length=job_length, cpu_required=(100-cpu_required), prob_cut_off=0.01, initial_train_size = 2000, update_freq=1)
-  write.csv(output$avg_usage, file = paste("ARMA", job_length, "100", 0.01, "avg_usage.csv"))
+  output <- svt_stationary_model(dataset=data_matrix, job_length=job_length, cpu_required=(100-cpu_required), prob_cut_off=0.1, initial_train_size = 2000, update_freq=1)
+  write.csv(output$avg_usage, file = paste("ARMA", job_length, "100", 0.1, "avg_usage.csv"))
   print(paste("Avg cycle used:", "job length", job_length, mean(as.matrix(output$avg_usage), na.rm = TRUE)))
-  write.csv(output$job_survival, file = paste("ARMA", job_length, "100", 0.01, "job_survival.csv"))
+  write.csv(output$job_survival, file = paste("ARMA", job_length, "100", 0.1, "job_survival.csv"))
   print(paste("Job survival rate:", "job length", job_length, sum(as.matrix(output$job_survival)) / (length(as.matrix(output$job_survival)))))
-  write.csv(output$scheduling_summary, file = paste("ARMA",job_length, "100", 0.01,"scheduling_sum.csv"))
+  write.csv(output$scheduling_summary, file = paste("ARMA",job_length, "100", 0.1,"scheduling_sum.csv"))
   scheduled_num <- sum(output$scheduling_summary[1,])
   unscheduled_num <- sum(output$scheduling_summary[2,])
   correct_scheduled_num <- scheduled_num - sum(output$scheduling_summary[3,])
