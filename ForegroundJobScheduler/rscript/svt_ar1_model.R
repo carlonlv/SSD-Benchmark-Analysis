609ffa841e04f0e7078db4ac2e5b94b248c384ad
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-08-18 16:44:38 -0400

    dd

--- ForegroundJobScheduler/rscript/svt_ar1_model.R
+++ ForegroundJobScheduler/rscript/svt_ar1_model.R
@@ -256,7 +256,7 @@ sample_size <- 3000
 window_size <- 12
 job_length <- 1
 cpu_usage <- 3
-prob_cut_off <- 0.01
+prob_cut_off <- 0.1
 total_trace_length <- 8000
 initial_train_size <- 6000
 mode <- 'max'
