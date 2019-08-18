dfbf7a74e90a7d3239bc905d645d605bdcc7da42
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-08-18 19:13:59 -0400

    Update mvt_stationary_model.R

--- ForegroundJobScheduler/rscript/mvt_stationary_model.R
+++ ForegroundJobScheduler/rscript/mvt_stationary_model.R
@@ -407,11 +407,13 @@ mvt_stationary_model <- function(dataset_avg, dataset_max, initial_train_size, p
 ## Read back ground job pool
 
 arg <- commandArgs(trailingOnly = TRUE)
-sample_size <- 100
-window_size <- 36
+sample_size <- 3000
+window_size <- 12
 job_length <- 1
 cpu_usage <- 3
-prob_cut_off <- 0.1
+prob_cut_off <- 0.01
+total_trace_length <- 8000
+initial_train_size <- 6000
 mode <- 'max'
 
 cat(arg, sep = "\n")
@@ -425,12 +427,12 @@ if (sample_size == 100 ) {
   bg_job_pool <- sub(".pd", "", bg_job_pool)
 }
 
-data_matrix_avg <- matrix(nrow = 4000, ncol = 0)
-data_matrix_max <- matrix(nrow = 4000, ncol = 0)
+data_matrix_avg <- matrix(nrow = total_trace_length, ncol = 0)
+data_matrix_max <- matrix(nrow = total_trace_length, ncol = 0)
 for (job_num in bg_job_pool) {
   bg_job <- read.csv(paste(bg_jobs_path, job_num, ".csv", sep = ""))
-  data_matrix_avg <- cbind(data_matrix_avg, bg_job$avg_cpu[4033:8032])
-  data_matrix_max <- cbind(data_matrix_max, bg_job$max_cpu[4033:8032])
+  data_matrix_avg <- cbind(data_matrix_avg, bg_job$avg_cpu[1:total_trace_length])
+  data_matrix_max <- cbind(data_matrix_max, bg_job$max_cpu[1:total_trace_length])
 }
 rownames(data_matrix_avg) <- seq(1, 1 + 5 * (nrow(data_matrix_avg) - 1),5)
 rownames(data_matrix_max) <- seq(1, 1 + 5 * (nrow(data_matrix_max) - 1),5)
@@ -442,7 +444,7 @@ for (j in 1:ncol(data_matrix_max)) {
   cpu_required[j] <- as.numeric(quantile(data_matrix_max[,j], c(0.15, 0.5, 0.85), type = 4)[cpu_usage])
 }
 
-output <- mvt_stationary_model(dataset_avg=data_matrix_avg, dataset_max = data_matrix_max, p=1, q=0,job_length=job_length, cpu_required=(100-cpu_required), prob_cut_off=prob_cut_off, initial_train_size = 2000, update_freq=1, mode = mode)
+output <- mvt_stationary_model(dataset_avg=data_matrix_avg, dataset_max = data_matrix_max, p=1, q=0,job_length=job_length, cpu_required=(100-cpu_required), prob_cut_off=prob_cut_off, initial_train_size = initial_train_size, update_freq=1, mode = mode)
 write.csv(output$avg_usage, file = paste("VARMA",window_size, sample_size, prob_cut_off, "avg_usage.csv"))
 print(paste("Avg cycle used:", "job length", window_size, mean(as.matrix(output$avg_usage), na.rm = TRUE)))
 write.csv(output$job_survival, file = paste("VARMA",window_size, sample_size, prob_cut_off,"job_survival.csv"))
