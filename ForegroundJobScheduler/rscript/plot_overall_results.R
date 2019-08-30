9d7021322b814fb1556680f33c8ee03078d4effe
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-08-29 20:56:22 -0400

    Update plot_overall_results.R

--- ForegroundJobScheduler/rscript/plot_overall_results.R
+++ ForegroundJobScheduler/rscript/plot_overall_results.R
@@ -13,24 +13,30 @@ read_from_models_xlsx <- function(model_results, xlsx, sample_size, window_size)
     for (alpha in current_model$Probability.Cut.Off) {
       current_model_prob <- current_model %>%
         filter(Probability.Cut.Off == alpha)
-      if (all(is.na(current_model_prob$StateNum))) {
+      for (granularity in current_model_prob$Granularity) {
+        current_model_prob_gran <- current_model_prob %>%
+          filter(Granularity == granularity)
         
-        utilization <- current_model_prob$Avg.Cycle.Usage
-        survival_rate <- current_model_prob$Survival.Rate
-        
-        result <- data.frame("Model" = model, "Prob_Cut_Off" = alpha, "StateNum" = 0, "Utilization" = utilization, "Survival" = survival_rate)
-        model_results <- rbind(model_results, result)
-      } else {
-        
-        for (stateNum in current_model_prob$StateNum) {
-          current_model_prob_stateNum <- current_model_prob %>%
-            filter(StateNum == stateNum)
-          utilization <- current_model_prob_stateNum$Avg.Cycle.Usage
-          survival_rate <- current_model_prob_stateNum$Survival.Rate
+        if (all(is.na(current_model_prob_gran$StateNum))) {
           
-          result <- data.frame("Model" = model, "Prob_Cut_Off" = alpha, "StateNum" = stateNum, "Utilization" = utilization, "Survival" = survival_rate)
+          utilization <- current_model_prob_gran$Avg.Cycle.Usage
+          survival_rate <- current_model_prob_gran$Survival.Rate
+          
+          result <- data.frame("Model" = model, "Prob_Cut_Off" = alpha, "StateNum" = 0, "Granularity" = granularity, "Utilization" = utilization, "Survival" = survival_rate)
           model_results <- rbind(model_results, result)
+        } else {
+          
+          for (stateNum in current_model_prob_gran$StateNum) {
+            current_model_prob_gran_stateNum <- current_model_prob_gran %>%
+              filter(StateNum == stateNum)
+            utilization <- current_model_prob_gran_stateNum$Avg.Cycle.Usage
+            survival_rate <- current_model_prob_gran_stateNum$Survival.Rate
+            
+            result <- data.frame("Model" = model, "Prob_Cut_Off" = alpha, "StateNum" = stateNum, "Granularity" = granularity, "Utilization" = utilization, "Survival" = survival_rate)
+            model_results <- rbind(model_results, result)
+          }
         }
+        
       }
     }
   }
@@ -39,8 +45,8 @@ read_from_models_xlsx <- function(model_results, xlsx, sample_size, window_size)
 
 plot_results <- function(model_results, sample_size, window_size) {
   ggplot(model_results, aes(x=Survival, y=Utilization, group=factor(Model), colour=factor(Model))) +
-    geom_point(na.rm=TRUE, aes(shape=as.factor(StateNum))) + 
-    scale_shape_manual(values=seq(0,6)) +
+    geom_point(na.rm=TRUE, aes(shape=factor(StateNum), alpha=factor(Granularity))) + 
+    scale_shape_manual(values=c(16,17,18,0,1,2,6)) +
     geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
     ylab("Utilization") +
     xlab("Survival Rate") + 
@@ -48,11 +54,11 @@ plot_results <- function(model_results, sample_size, window_size) {
   ggsave(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size, ".png"))
 }
 
-#ar_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary (windows) max.xlsx"
-ar_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary (windows) max post adj.xlsx"
-mc_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//mc summary.xlsx"
-sample_size <- 3000
-window_size <- 36
+ar_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary (windows,granularity).xlsx"
+#ar_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary (windows,granularity) post adj.xlsx"
+#mc_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//mc summary.xlsx"
+sample_size <- 100
+window_size <- 12
 
 ar_xlsx <- read.xlsx(ar_data_path, sheetIndex = 1)
 #mc_xlsx <- read.xlsx(mc_data_path, sheetIndex = 1)
