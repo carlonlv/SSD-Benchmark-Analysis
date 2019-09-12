49ab32e408cbf785e02053975788eafdf88b2a76
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-09-12 09:32:54 -0400

    dasd

--- ForegroundJobScheduler/rscript/plot_individual_results.R
+++ ForegroundJobScheduler/rscript/plot_individual_results.R
@@ -8,7 +8,7 @@ calculate_utilization <- function(utilization_info) {
 }
 
 calculate_survival <- function(survival_info) {
-  return(sum(survival_info, na.rm = TRUE) / length(survival_info[!is.na(survival_info)]))
+  return(mean(survival_info, na.rm = TRUE))
 }
 
 find_runs <- function(survival_for_trace) {
@@ -16,9 +16,9 @@ find_runs <- function(survival_for_trace) {
   current_pos <- 1
   counter <- 0
   while (current_pos <= length(survival_for_trace)) {
-    if (survival_for_trace[current_pos] == 0) {
+    if (!is.na(survival_for_trace[current_pos]) & survival_for_trace[current_pos] == 0) {
       counter <- counter + 1
-    } else if (counter > 0) {
+    } else if (!is.na(survival_for_trace[current_pos]) & counter > 0) {
       count_of_runs[counter] <- count_of_runs[counter] + 1
       counter <- 0
     }
@@ -28,51 +28,74 @@ find_runs <- function(survival_for_trace) {
 }
 
 compute_distribution_of_runs <- function(survival_info) {
-  total_count_of_runs <- rep(0, nrow(survival_info))
   result.lst <- apply(survival_info, 2, find_runs)
-  
+  ## Here add to total_count_of_runs
+  return(rowSums(result.lst))
 }
 
-read_from_ar_result <- function(model, window_size, sample_size, prob_cut_off, state_num, granularity, datapath) {
+read_from_ar_result <- function(model, window_size, sample_size, prob_cut_off, state_num, granularity, cpu_usage, datapath, mode="raw") {
   utilization_filename <- NULL
   survival_filename <- NULL
-  if (is.na(state_num)) {
-    utilization_filename <- paste(model, window_size, sample_size, prob_cut_off, granularity, "avg_usage.csv")
-    survival_filename <- paste(model, window_size, sample_size, prob_cut_off, granularity, "job_survival.csv")
-  } else {
+  if (!is.na(state_num)) {
     utilization_filename <- paste(model, window_size, state_num, sample_size, prob_cut_off, granularity, "avg_usage.csv")
     survival_filename <- paste(model, window_size, state_num, sample_size, prob_cut_off, granularity, "job_survival.csv")
+  } else if (!is.na(cpu_usage)){
+    utilization_filename <- paste(model, window_size, sample_size, prob_cut_off, granularity, cpu_usage,"avg_usage.csv")
+    survival_filename <- paste(model, window_size, sample_size, prob_cut_off, granularity, cpu_usage,"job_survival.csv")
+  } else {
+    utilization_filename <- paste(model, window_size, sample_size, prob_cut_off, granularity,"avg_usage.csv")
+    survival_filename <- paste(model, window_size, sample_size, prob_cut_off, granularity,"job_survival.csv")
   }
+  utilization_dp <- paste(datapath, utilization_filename, sep = "")
   survival_dp <- paste(datapath, survival_filename, sep = "")
-  utilization_info <- read.csv(utilization_dp)
-  survival_info <- read.csv(survival_dp)
+  utilization_info <- read.csv(utilization_dp)[-1]
+  survival_info <- read.csv(survival_dp)[-1]
   
-  
-  utilization_info <- utilization_info %>%
-    summarise_all(funs(calculate_utilization))
-  utilization_info <- as.numeric(utilization_info[1,])
-  survival_info <- survival_info %>%
-    summarise_all(funs(calculate_survival))
-  survival_info <- as.numeric(survival_info[1,])
-  return(list("utilization_info"=utilization_info, "survival_info"=survival_info))
+  if (mode == "raw") {
+    return(list("utilization_info"=utilization_info, "survival_info"=survival_info))
+  } else if (mode == "both"){
+    utilization_summary <- utilization_info %>%
+      summarise_all(funs(calculate_utilization))
+    utilization_summary <- as.numeric(utilization_summary[1,])
+    survival_summary<- survival_info %>%
+      summarise_all(funs(calculate_survival))
+    survival_summary <- as.numeric(survival_summary[1,])
+    return(list("utilization_info"=utilization_info, "survival_info"=survival_info, "utilization_summ"=utilization_summary, "survivial_summ"=survival_summary))
+  } else {
+    utilization_summary <- utilization_info %>%
+      summarise_all(funs(calculate_utilization))
+    utilization_summary <- as.numeric(utilization_summary[1,])
+    survivial_summ <- survival_info %>%
+      summarise_all(funs(calculate_survival))
+    survivial_summ <- as.numeric(survivial_summ[1,])
+    return(list("utilization_summ"=utilization_info, "survivial_summ"=survival_info))
+  }
 }
 
-make_histogram <- function(info_df) {
-  ggplot(info_df, aes(x=Survival, y=Utilization, group=factor(Model), colour=factor(Model))) +
-    geom_point(na.rm=TRUE, aes(shape=as.factor(StateNum))) + 
-    geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
-    ylab("Utilization") +
-    xlab("Survival Rate") + 
-    ggtitle(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size))
-  ggsave(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size, ".png"))
+make_plot <- function(info_df, sample_size, window_size) {
+  ## Here plot a histogram
+  ggplot(info_df, aes(x=x, y=y)) +
+    geom_point() + 
+    ylab("Count of Runs") +
+    xlab("Runs") + 
+    ggtitle(paste("Runs for trace", sample_size, "and Window Size", window_size))
+  ggsave(paste("Runs for trace", sample_size, "and Window Size", window_size, ".png"))
 }
 
-result_folder_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//maxes"
-model_name <- "AR1"
+result_folder_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//maxes//"
+model_name <- "AR1_logistic_lm"
 window_size <- 12
-sample_size <- 100
+sample_size <- 3000
 prob_cut_off <- 0.01
-granularity <- 10
+granularity <- 0.78125
 state_num <- NA
+cpu_usage <- 1
 
-model_performance <- read_from_ar_result(model_name, window_size, sample_size, prob_cut_off, state_num, granularity, result_folder_path)
+model_performance <- read_from_ar_result(model_name, window_size, sample_size, prob_cut_off, state_num, granularity, cpu_usage, result_folder_path, mode="raw")
+
+## Check traces performance
+
+## Check histogram of runs
+distribution_of_runs <- compute_distribution_of_runs(model_performance$survival_info)[1:50]
+distribution_of_runs.df <- data.frame(x=1:length(distribution_of_runs), y=distribution_of_runs)
+make_plot(distribution_of_runs.df, sample_size, window_size)
--- ForegroundJobScheduler/rscript/plot_overall_results.R
+++ ForegroundJobScheduler/rscript/plot_overall_results.R
@@ -3,34 +3,42 @@ library("readxl")
 library("ggplot2")
 library("dplyr")
 
-plot_results <- function(model_results, sample_size, window_size) {
+plot_results <- function(model_results, sample_size, window_size, model_name="NULL") {
   model_results <- model_results %>% 
     filter(Sample.Size== sample_size & Window.Size == window_size)
-  ggplot(model_results, aes(x=Survival.Rate, y=Avg.Cycle.Usage)) +
-    geom_point(na.rm=TRUE, aes(color=factor(Model), shape=factor(StateNum), alpha=factor(Granularity))) + 
-    scale_shape_manual(values=c(16,17,18,21,22,23,24)) +
-    geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
-    ylab("Utilization") +
-    xlab("Survival Rate") + 
-    ggtitle(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size))
-  ggsave(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size, ".png"))
+  if (model_name == "ar_logistic_lm") {
+    ggplot(model_results, aes(x=Survival.Rate, y=Avg.Cycle.Usage)) +
+      geom_point(na.rm=TRUE, aes(color=factor(Model), shape=factor(Cpu_usage), alpha=factor(Granularity))) + 
+      geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
+      ylab("Utilization") +
+      xlab("Survival Rate") + 
+      ggtitle(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size))
+    ggsave(paste("Model Performance of AR_Logistic_Lm With Sample Size", sample_size, "and Window Size", window_size, ".png"))
+  } else {
+    ggplot(model_results, aes(x=Survival.Rate, y=Avg.Cycle.Usage)) +
+      geom_point(na.rm=TRUE, aes(color=factor(Model), shape=factor(StateNum), alpha=factor(Granularity))) + 
+      scale_shape_manual(values=c(15, 16,17,18,21,22,23,24)) +
+      geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
+      ylab("Utilization") +
+      xlab("Survival Rate") + 
+      ggtitle(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size))
+    ggsave(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size, ".png"))
+  }
 }
 
-#ar_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary (windows,granularity).xlsx"
+ar_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary (windows,granularity).xlsx"
 #ar_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary (windows,granularity) post adj.xlsx"
-mc_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//mc summary granularity.csv"
+#mc_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//mc summary granularity.csv"
 sample_size <- 3000
-window_size <- 12
+window_size <- 36
 
-#ar_xlsx <- read.xlsx(ar_data_path, sheetIndex = 1)
-#ar_xlsx <- ar_xlsx %>%
-  #mutate(StateNum=ifelse(is.na(StateNum), 0, StateNum))
+ar_xlsx <- read.xlsx(ar_data_path, sheetIndex = 1)
+#mc_xlsx <- read.csv(mc_data_path)
+#colnames(mc_xlsx) <- c("Model", "Survival.Rate", "Avg.Cycle.Usage", "Sample.Size", "Window.Size", "StateNum","Probability.Cut.Off")
 
-mc_xlsx <- read.csv(mc_data_path)
-colnames(mc_xlsx) <- c("Model", "Survival.Rate", "Avg.Cycle.Usage", "Sample.Size", "Window.Size", "StateNum","Probability.Cut.Off")
+plot_results(ar_xlsx, sample_size, window_size)
+#plot_results(mc_xlsx, sample_size, window_size)
 
-#model_results <- data.frame(matrix(nrow = 0, ncol = 5), stringsAsFactors = FALSE)
-#colnames(model_results) <- c("Model", "Prob_Cut_Off",  "StateNum", "Utilization", "Survival")
-#model_results <- read_from_models_xlsx(model_results, ar_xlsx, sample_size, window_size)
-
-plot_results(mc_xlsx, sample_size, window_size)
+ar_logistic_lm_data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary AR1_logistic_lm.xlsx"
+ar_logistic_lm_xlsx <- read.xlsx(ar_logistic_lm_data_path, sheetIndex = 1)
+plot_results(ar_logistic_lm_xlsx, sample_size, window_size, "ar_logistic_lm")
