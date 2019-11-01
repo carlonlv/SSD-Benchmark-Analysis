0dd9fa06b7186dccadceb166d1efa4e4a587b290
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-11-01 14:36:02 -0400

    lalalalal

--- ForegroundJobScheduler/rscript/plot_overall_results.R
+++ ForegroundJobScheduler/rscript/plot_overall_results.R
@@ -3,29 +3,67 @@ library("readxl")
 library("ggplot2")
 library("dplyr")
 
-plot_results <- function(model_results, sample_size, window_size, model_name, usage_calculation=1) {
+plot_results <- function(model_results, sample_size, window_size, model_name, utilization_type=2) {
   model_results <- model_results %>% 
     filter(Sample.Size== sample_size & Window.Size == window_size & Model %in% model_name)  
-  ggplot(model_results, aes(x=Survival.Rate, y=Avg.Cycle.Usage1)) +
-    geom_point(na.rm=TRUE, aes(color=factor(BinNum), fill=factor(Model), shape=factor(StateNum), alpha=factor(Granularity))) + 
-    scale_shape_manual(values=c(21,22,23,24,25)) +
-    geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
-    ylab("Utilization") +
-    xlab("Survival Rate") + 
-    ggtitle(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size))
+  if (utilization_type == 1) {
+    ggplot(model_results, aes(x=Survival.Rate, y=Avg.Cycle.Usage1)) +
+      geom_point(na.rm=TRUE, aes(color=factor(Model), size=factor(BinNum), shape=factor(StateNum), alpha=factor(Granularity))) + 
+      scale_shape_manual(values=c(21,22,23,24,25)) +
+      geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
+      ylab("Utilization") +
+      xlab("Survival Rate") + 
+      ggtitle(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size))
+  } else {
+    ggplot(model_results, aes(x=Survival.Rate, y=Avg.Cycle.Usage2)) +
+      geom_point(na.rm=TRUE, aes(color=factor(Model), size=factor(BinNum), shape=factor(StateNum), alpha=factor(Granularity))) + 
+      scale_shape_manual(values=c(21,22,23,24,25)) +
+      geom_vline(xintercept=0.99, linetype="dashed", color="red") + 
+      ylab("Utilization") +
+      xlab("Survival Rate") + 
+      ggtitle(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size))
+  }
   ggsave(paste("Model Performance With Sample Size", sample_size, "and Window Size", window_size, ".png"))
-}
-
-data_path <- NULL
-if (Sys.info()["sysname"] == "Windows") {
-  data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//offline results//summary dynamic (windows,granularity).xlsx"
-} else {
-  data_path <- "/Users/carlonlv/Documents/GitHub/Research-Projects/ForegroundJobScheduler/results/offline results/summary dynamic (windows,granularity).xlsx"
 }
 
 sample_size <- 100
 window_size <- 12
+adjustment <- FALSE
+schedule_policy <- "dynamic"
+
+data_path <- NULL
+if (adjustment) {
+  if (schedule_policy == "dynamic") {
+    if (Sys.info()["sysname"] == "Windows") {
+      data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//offline results//summary dynamic (windows,granularity) post adj.xlsx"
+    } else {
+      data_path <- "/Users/carlonlv/Documents/Github/Research-Projects/ForegroundJobScheduler/results/offline results/summary dynamic (windows,granularity) post adj.xlsx"
+    }
+  } else {
+    if (Sys.info()["sysname"] == "Windows") {
+      data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//offline results//summary disjoint (windows,granularity) post adj.xlsx"
+    } else {
+      data_path <- "/Users/carlonlv/Documents/Github/Research-Projects/ForegroundJobScheduler/results/offline results/summary disjoint (windows,granularity) post adj.xlsx"
+    }
+  }
+} else {
+  if (schedule_policy == "dynamic") {
+    if (Sys.info()["sysname"] == "Windows") {
+      data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//offline results//summary dynamic (windows,granularity).xlsx"
+    } else {
+      data_path <- "/Users/carlonlv/Documents/Github/Research-Projects/ForegroundJobScheduler/results/offline results/summary dynamic (windows,granularity).xlsx"
+    }
+  } else {
+    if (Sys.info()["sysname"] == "Windows") {
+      data_path <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//offline results//summary disjoint (windows,granularity).xlsx"
+    } else {
+      data_path <- "/Users/carlonlv/Documents/Github/Research-Projects/ForegroundJobScheduler/results/offline results/summary disjoint (windows,granularity).xlsx"
+    }
+  }
+}
+
+model_names <- c("AR1", "VAR1", "Markov", "AR1_Markov", "AR1_state_based_logistic", "AR1_logistic_glm")
 
 ar_xlsx <- read.xlsx(data_path, sheetIndex = 1)
-plot_results(ar_xlsx, sample_size, window_size, model_name = c("AR1_logistic_glm", "AR1", "VAR1"))
+plot_results(ar_xlsx, sample_size, window_size, model_name = model_names[c(1,3,4)])
 
