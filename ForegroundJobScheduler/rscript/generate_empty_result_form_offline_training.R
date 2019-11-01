a6de539a80010f24cceebc65d8d4c516e475f7bd
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-11-01 01:14:01 -0400

    Updates

--- ForegroundJobScheduler/rscript/generate_empty_result_form_offline_training.R
+++ ForegroundJobScheduler/rscript/generate_empty_result_form_offline_training.R
@@ -3,16 +3,16 @@ library("xlsx")
 
 models <- c("AR1", "VAR1", "AR1_logistic_glm", "AR1_logistic_lm", "AR1_state_based_logistic", "AR1_Markov", "Markov")
 statenum <- c(32, 64, 128)
-prob_cut_offs <- c(0.01, 0.1)
-granularity <- c(100/32, 0)
+prob_cut_offs <- c(0.005, 0.01, 0.1, 0.75)
+granularity <- c(100/32, 100/64, 100/128, 0)
 window_size <- c(12, 36)
 sample_size <- c(100, 3000)
 bin_num <- c(1000, 500)
 
-prob_ban_pool <- c(0.5, 0.75)
+prob_ban_pool <- c(0.75)
 
-result.dp1 <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary disjoint (windows,granularity).xlsx"
-result.dp2 <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//summary disjoint (windows,granularity) post adj.xlsx"
+result.dp1 <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//offline results//summary disjoint (windows,granularity).xlsx"
+result.dp2 <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//offline results//summary disjoint (windows,granularity) post adj.xlsx"
 result.df <- expand.grid(models, statenum, prob_cut_offs, granularity, window_size, sample_size, bin_num, KEEP.OUT.ATTRS=FALSE, stringsAsFactors=FALSE)
 colnames(result.df) <- c("Model", "StateNum", "Probability.Cut.Off", "Granularity", "Window.Size", "Sample.Size", "BinNum")
 result.df$Avg.Cycle.Usage1 <- NA
---
+++ ForegroundJobScheduler/rscript/generate_empty_result_form_online_training.R
@@ -0,0 +1,41 @@
+library("dplyr")
+library("xlsx")
+
+models <- c("AR1", "VAR1", "AR1_logistic_glm", "AR1_logistic_lm", "AR1_state_based_logistic", "AR1_Markov", "Markov")
+statenum <- c(32, 64)
+prob_cut_offs <- c(0.01, 0.1)
+granularity <- c(100/32, 0)
+window_size <- c(12, 36)
+sample_size <- c(100, 3000)
+bin_num <- c(1000, 500)
+train_size <- c(2000, 4000)
+
+result.dp1 <- NULL
+result.dp2 <- NULL
+if (Sys.info()["sysname"] == "Windows") {
+  result.dp1 <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//online results//summary disjoint (windows,granularity).xlsx"
+  result.dp2 <- "C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//Nonoverlapping windows//online results//summary disjoint (windows,granularity) post adj.xlsx"
+} else {
+  result.dp1 <- "/Users/carlonlv/Documents/Github/Research-Projects/ForegroundJoScheduler/results/Nonoverlapping windows/online results/summary disjoint (windows,granularity).xlsx"
+  result.dp2 <- "/Users/carlonlv/Documents/Github/Research-Projects/ForegroundJoScheduler/results/Nonoverlapping windows/online results/summary disjoint (windows,granularity) post adj.xlsx"
+}
+
+
+result.df <- expand.grid(models, statenum, prob_cut_offs, granularity, window_size, sample_size, bin_num, train_size, KEEP.OUT.ATTRS=FALSE, stringsAsFactors=FALSE)
+colnames(result.df) <- c("Model", "StateNum", "Probability.Cut.Off", "Granularity", "Window.Size", "Sample.Size", "BinNum", "Training Size")
+result.df$Update.Freq <- 3 * result.df$Window.Size
+result.df$Avg.Cycle.Usage1 <- NA
+result.df$Avg.Cycle.Usage2 <- NA
+result.df$Survival.Rate <- NA
+result.df$Correctly.Scheduled <- NA
+result.df$Correctly.Unscheduled <- NA
+
+result.df <- result.df %>%
+  mutate(StateNum=ifelse(Model=="AR1_state_based_logistic", StateNum, 0)) %>%
+  mutate(BinNum=ifelse(Model=="AR1_logistic_lm" | Model == "AR1_logistic_glm", BinNum, 0)) %>%
+  filter(!is.na(Probability.Cut.Off)) %>%
+  distinct() %>%
+  arrange(Model, StateNum, Probability.Cut.Off, Granularity, Window.Size, Sample.Size)
+
+write.xlsx(result.df, file = result.dp1, row.names = FALSE, showNA = FALSE)
+write.xlsx(result.df, file = result.dp2, row.names = FALSE, showNA = FALSE)
