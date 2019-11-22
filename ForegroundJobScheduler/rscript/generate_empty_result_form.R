93e7bfe03274df08b173bc6ad55e458c5f63721e
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-11-22 14:26:28 -0500

    asddd

---
+++ ForegroundJobScheduler/rscript/generate_empty_result_form.R
@@ -0,0 +1,43 @@
+library("dplyr")
+
+models <- c("AR1", "VAR1", "AR1_logistic_glm", "AR1_logistic_lm", "AR1_state_based_logistic", "AR1_Markov", "Markov")
+statenum <- c(8, 16, 32, 64)
+prob_cut_offs <- c(0.0025, 0.005, 0.01, 0.1)
+granularity <- c(100/32, 100/64, 100/128, 0)
+window_size <- c(12, 36)
+sample_size <- c(100, 3000)
+bin_num <- c(1000, 500)
+
+mode <- "offline"
+schedule_policy <- "disjoint"
+
+result.dp1 <- NULL
+result.dp2 <- NULL
+if (Sys.info()["sysname"] == "Windows") {
+  result.dp1 <- paste0("C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//", paste(mode, "results"), "//", paste("summary", schedule_policy), ".csv")
+  result.dp2 <- paste0("C://Users//carlo//Documents//GitHub//Research-Projects//ForegroundJobScheduler//results//", paste(mode, "results"), "//", paste("summary", schedule_policy, "post adj"), ".csv")
+} else if (Sys.info()["sysname"] == "Darwin") {
+  result.dp1 <- paste0("/Users/carlonlv/Documents/GitHub/Research-Projects/ForegroundJobScheduler/results/", paste(mode, "results"), "/", paste("summary", schedule_policy), ".csv")
+  result.dp2 <- paste0("/Users/carlonlv/Documents/GitHub/Research-Projects/ForegroundJobScheduler/results/", paste(mode, "results"), "/", paste("summary", schedule_policy, "post adj"), ".csv")
+} else {
+  result.dp1 <- paste0("/home/jialun/Research-Projects/ForegroundJobScheduler/results/", paste(mode, "results"), "/", paste("summary", schedule_policy), ".csv")
+  result.dp2 <- paste0("/home/jialun/Research-Projects/ForegroundJobScheduler/results/", paste(mode, "results"), "/", paste("summary", schedule_policy, "post adj"), ".csv")
+}
+
+result.df <- expand.grid(models, statenum, prob_cut_offs, granularity, window_size, sample_size, bin_num, KEEP.OUT.ATTRS=FALSE, stringsAsFactors=FALSE)
+colnames(result.df) <- c("Model", "StateNum", "Probability.Cut.Off", "Granularity", "Window.Size", "Sample.Size", "BinNum")
+result.df$Avg.Cycle.Usage <- NA
+result.df$Agg.Cycle.Usage <- NA
+result.df$Avg.Survival.Rate <- NA
+result.df$Agg.Survival.Rate <- NA
+result.df$Correctly.Scheduled <- NA
+result.df$Correctly.Unscheduled <- NA
+
+result.df <- result.df %>%
+  mutate(StateNum=ifelse(Model %in% c("AR1_state_based_logistic", "Markov", "AR1_Markov"), StateNum, 0)) %>%
+  mutate(BinNum=ifelse(Model %in% c("AR1_logistic_lm", "AR1_logistic_glm"), BinNum, 0)) %>%
+  distinct() %>%
+  arrange(Model, StateNum, Probability.Cut.Off, Granularity, Window.Size, Sample.Size)
+
+write.csv(result.df, file = result.dp1, row.names = FALSE)
+write.csv(result.df, file = result.dp2, row.names = FALSE)
