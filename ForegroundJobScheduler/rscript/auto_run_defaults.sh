0a5db0a669470744a61c5d01bfe3939ec5a6eb88
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-11-26 09:54:34 -0500

    Update auto_run_defaults.sh

--- ForegroundJobScheduler/rscript/auto_run_defaults.sh
+++ ForegroundJobScheduler/rscript/auto_run_defaults.sh
@@ -2,7 +2,7 @@ echo Which model to run?
 read varname
 Rscript run_sim.R --action file --sim offline --sample 100 --schedule dynamic --file default --result True --adjust TRUE --model $varname
 Rscript run_sim.R --action file --sim offline --sample 100 --schedule dynamic --file default --result True --adjust FALSE --model $varname
-Rscript run_sim.R --action file --sim offline --sample 100 --schedule disjoint --file default --result True --adjust TRUE --model $varname
+Rscript run_sim.R --action file --sim offline --sample 100 --schedule disjoint --file default --result True --adjust FALSE --model $varname
 Rscript run_sim.R --action file --sim online --sample 100 --schedule dynamic --file default --result True --adjust TRUE --model $varname
 Rscript run_sim.R --action file --sim online --sample 100 --schedule dynamic --file default --result True --adjust FALSE --model $varname
-Rscript run_sim.R --action file --sim online --sample 100 --schedule disjoint --file default --result True --adjust TRUE --model $varname
+Rscript run_sim.R --action file --sim online --sample 100 --schedule disjoint --file default --result True --adjust FALSE --model $varname
