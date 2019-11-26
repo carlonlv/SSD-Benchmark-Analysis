5b08fa46e9b0ba2ed4bbef6aa48daa087ec5aa7a
root <root@swift-071.syslab.sandbox>
2019-11-25 22:19:19 -0500

    ddd

--- ForegroundJobScheduler/rscript/set_up.sh
+++ ForegroundJobScheduler/rscript/set_up.sh
@@ -1,5 +1,11 @@
 sudo su
-sudo apt-get update 
-sudo apt-get install r-base
-Rscript
-Rscript /home/jialun/Research-Projects/ForegroundJobScheduler/rscript/config_packages.R
+sudo apt install apt-transport-https software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+
+sudo apt update
+sudo apt install r-base
+
+sudo apt install libssl-dev
+sudo apt install libxml2-dev
+
