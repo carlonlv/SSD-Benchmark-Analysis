4fd400ce81868f6e606afd3f63df6f0b16276309
carlonlv <36804238+carlonlv@users.noreply.github.com>
2019-11-28 08:31:23 -0500

    Update Merge_Data_Frame.sh

--- ForegroundJobScheduler/rscript/Merge_Data_Frame.sh
+++ ForegroundJobScheduler/rscript/Merge_Data_Frame.sh
@@ -15,4 +15,7 @@ Rscript MergeResultForm.R --from ../results/offline\ results/temp_disjoint1.csv 
 git status
 git add ../results/online\ results/summary\ dynamic.csv
 git add ../results/online\ results/summary\ dynamic\ post\ adj.csv
-git add ../results/online\ results/summary\ dynamic.csv
+git add ../results/online\ results/summary\ disjoint.csv
+git add ../results/offline\ results/summary\ dynamic.csv
+git add ../results/offline\ results/summary\ dynamic\ post\ adj.csv
+git add ../results/offline\ results/summary\ disjoint.csv
