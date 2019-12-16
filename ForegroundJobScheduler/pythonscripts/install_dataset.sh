1cf1b622e33b75d9cc0cc66b5785bea0726f59c0
root <root@swift-070.syslab.sandbox>
2019-12-16 16:30:24 -0500

    dd

---
+++ ForegroundJobScheduler/pythonscripts/install_dataset.sh
@@ -0,0 +1,9 @@
+filename='url.txt'
+output='/home/jialun/microsoft_traces/'
+n=1
+while read line; do
+# reading each line
+echo "Line No. $n"
+wget --directory-prefix= $output $line
+n=$((n+1))
+done < $filename
