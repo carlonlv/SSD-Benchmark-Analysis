4e6f2e0687081a3cca747413123e795c7fb858bf
carlonlv <36804238+carlonlv@users.noreply.github.com>
2020-06-25 19:33:19 -0700

    Update

--- ForegroundJobScheduler/rscripts/plot_sim_result.R
+++ ForegroundJobScheduler/rscripts/plot_sim_result.R
@@ -3,13 +3,13 @@ library("dplyr")
 
 overall_df <- data.frame()
 feature <- "name"
-for (i in 1:5) {
+for (i in 1:4) {
   feature_df <- read.csv(file.choose())
   paramwise_df <- read.csv(file.choose())
   overall_df <- rbind(overall_df, cbind(paramwise_df, feature = feature_df[1, feature]))
 }
 
-plot_ecdf_traces_performance(overall_df, "Model", FALSE, "ARI11,fixed", "~/Documents/")
+plot_ecdf_traces_performance(overall_df, "Model", FALSE, "Markov_fixed,Markov_quantile,MarkovX_fixed,MarkovX_quantile", "~/Documents/")
 
 
 overall_df <- data.frame()
