ea8d3d40713bc78cfb24ae433b0ac7b9e0bf7273
carlonlv <36804238+carlonlv@users.noreply.github.com>
2020-08-25 22:39:19 -0700

    Update

--- ForegroundJobScheduler/rscripts/plot_sim_result.R
+++ ForegroundJobScheduler/rscripts/plot_sim_result.R
@@ -2,22 +2,22 @@ library("DataCenterSim")
 library("dplyr")
 
 overall_df <- data.frame()
-feature <- "name"
-for (i in 1:5) {
+feature <- "granularity"
+for (i in 1:4) {
   feature_df <- read.csv(file.choose())
   paramwise_df <- read.csv(file.choose())
   overall_df <- rbind(overall_df, cbind(paramwise_df, feature = feature_df[1, feature]))
 }
 
-plot_ecdf_traces_performance(overall_df, "Model", FALSE, "AR1,VAR1,Markov8,NN,NNX", "~/Documents/")
+plot_ecdf_traces_performance(overall_df, "Granularity", FALSE, "AR1 Model Offline Training Policy", "~/Documents/")
 
 
 overall_df <- data.frame()
-feature <- "cut_off_prob"
+feature <- "granularity"
 feature_df <- read.csv(file.choose())
-for (i in 1:5) {
+for (i in 1:4) {
   paramwise_df <- read.csv(file.choose())
   overall_df <- rbind(overall_df, cbind(paramwise_df, feature = feature_df[i, feature]))
 }
 
-plot_ecdf_traces_performance(overall_df, "Cut Off Prob", FALSE, "ARI11,fixed", "~/Documents/")
+plot_ecdf_traces_performance(overall_df, "granularity", FALSE, "AR1 Model Offline Training Policy", "~/Documents/")
