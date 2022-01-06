c04718dcf6bd6fb729c7aadac0811bcf2098abdc
carlonlv <36804238+carlonlv@users.noreply.github.com>
2022-01-05 23:29:51 -0500

    Update

--- ForegroundJobScheduler/pythonscripts/install_dataset.sh
+++
@@ -1,9 +0,0 @@
-filename='url.txt'
-output='/home/jialun/microsoft_traces/'
-n=1
-while read line; do
-# reading each line
-echo "Line No. $n"
-wget --directory-prefix=$output $line
-n=$((n+1))
-done < $filename
